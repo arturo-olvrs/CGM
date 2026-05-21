6dface71ae0141f3874cf240b7921f8b33d51709
arturo-olvrs <2004.olivares@gmail.com>
2026-05-21 09:33:05 +0200

    super sampling activated

--- 01_Raytracing/main.cpp
+++ 01_Raytracing/main.cpp
@@ -13,7 +13,7 @@ public:
     
   virtual void init() {
     GL(glDisable(GL_CULL_FACE));
-    render(Scene::genSimpleScene(), 7);
+    render(Scene::genSimpleScene(), 9);
   }
 
   void render(Scene scene, int depth) {
@@ -23,8 +23,7 @@ public:
 
       // instantiates the actual renderer with recursion depth 9 and 9x super sampling
       // NOTE: you might want to reduce the super sampling to 1 for testing and debugging
-      Raytracer renderer(depth, 1);
-      // TODO: Set back to 9
+      Raytracer renderer(depth, 9);
       renderer.setCamera(camera);
       renderer.setScene(scene);
       renderer.render(image);
